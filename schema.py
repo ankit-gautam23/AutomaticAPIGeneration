04c1632bfc9d192327da88b4de3c15d4eba4c042
Ankit Gautam <96822153+ankit-gautam23@users.noreply.github.com>
2023-04-17 19:14:48 +0530

    Update schema.py

--- schema.py
+++ schema.py
@@ -1,1 +1,20 @@
+user_type = GraphQLObjectType(
+    name='User',
+    fields={
+        'id': GraphQLField(GraphQLString),
+        'name': GraphQLField(GraphQLString)
+    }
+)
 
+query_type = GraphQLObjectType(
+    name='Query',
+    fields={
+        'users': GraphQLField(
+            GraphQLList(user_type),
+            resolver=lambda root, info: conn.cursor().execute('SELECT * FROM users').fetchall()
+        )
+    }
+)
+
+schema = GraphQLSchema(query=query_type)
+app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
