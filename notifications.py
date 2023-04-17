f5f39b47115281d8051f43f203250786630ed1f6
Ankit Gautam <96822153+ankit-gautam23@users.noreply.github.com>
2023-04-17 19:17:53 +0530

    Update notifications.py

--- notifications.py
+++ notifications.py
@@ -5,3 +5,24 @@ def handle_connect():
 @socketio.on('disconnect')
 def handle_disconnect():
     print('Client disconnected')
+    
+@socketio.on('subscribe')
+def handle_subscribe():
+    conn.cursor().execute('LISTEN user_changed')
+
+
+CREATE OR REPLACE FUNCTION notify_user_changed()
+RETURNS trigger AS
+$$
+BEGIN
+    PERFORM pg_notify('user_changed', NEW.id::text);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER user_changed_trigger
+AFTER INSERT OR UPDATE OR DELETE ON users
+FOR EACH ROW
+EXECUTE PROCEDURE notify_user_changed();
+
+
