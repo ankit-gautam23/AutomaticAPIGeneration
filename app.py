e134e62ce9602c49d7675f7be8bae3d65bbe7057
Ankit Gautam <96822153+ankit-gautam23@users.noreply.github.com>
2023-04-17 19:10:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from flask_socketio import SocketIO, emit
+from flask_graphql import GraphQLView
+from flask_restful import Api
+from graphql import GraphQLSchema, GraphQLObjectType, GraphQLString
+import psycopg2
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your-secret-key'
+socketio = SocketIO(app)
+api = Api(app)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+# Define GraphQL types and resolvers here
+# ...
+
+# Define RESTful API resources here
+# ...
