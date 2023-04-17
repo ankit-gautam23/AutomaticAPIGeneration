70db29c04f3551a18d253ab16a0a0047ccb202c8
Ankit Gautam <96822153+ankit-gautam23@users.noreply.github.com>
2023-04-17 19:20:02 +0530

    Update notifications.py

--- notifications.py
+++ notifications.py
@@ -11,18 +11,18 @@ def handle_subscribe():
     conn.cursor().execute('LISTEN user_changed')
 
 
-CREATE OR REPLACE FUNCTION notify_user_changed()
-RETURNS trigger AS
-$$
-BEGIN
-    PERFORM pg_notify('user_changed', NEW.id::text);
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
+# CREATE OR REPLACE FUNCTION notify_user_changed()
+# RETURNS trigger AS
+# $$
+# BEGIN
+#     PERFORM pg_notify('user_changed', NEW.id::text);
+#     RETURN NEW;
+# END;
+# $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER user_changed_trigger
-AFTER INSERT OR UPDATE OR DELETE ON users
-FOR EACH ROW
-EXECUTE PROCEDURE notify_user_changed();
+# CREATE TRIGGER user_changed_trigger
+# AFTER INSERT OR UPDATE OR DELETE ON users
+# FOR EACH ROW
+# EXECUTE PROCEDURE notify_user_changed();
 
 
