73f1e3f42cd369b9a60845720dcd2b7bc2ef7685
Ankit Gautam <96822153+ankit-gautam23@users.noreply.github.com>
2023-04-17 19:14:03 +0530

    Update resources.py

--- resources.py
+++ resources.py
@@ -1,1 +1,18 @@
-
+class User(Resource):
+    def get(self, user_id):
+        # Retrieve user from database and return as JSON
+        return {'id': user_id, 'name': 'John Doe'}
+    
+    def post(self):
+        # Insert new user into database and return as JSON
+        return {'id': 1, 'name': 'John Doe'}
+    
+    def put(self, user_id):
+        # Update user in database and return as JSON
+        return {'id': user_id, 'name': 'John Doe'}
+    
+    def delete(self, user_id):
+        # Delete user from database and return success message
+        return {'message': 'User deleted successfully'}
+    
+api.add_resource(User, '/users', '/users/<int:user_id>')
